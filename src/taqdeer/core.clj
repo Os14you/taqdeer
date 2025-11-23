b64d11fe0c99968cc8d928a6cf759bb2b513a01c
Osama Yousef <osama.yousef@tuta.io>
2025-11-23 22:55:04 +0200

    feat: add config.edn file

--- src/taqdeer/core.clj
+++ src/taqdeer/core.clj
@@ -1,17 +1,13 @@
 (ns taqdeer.core
+  (:require [clojure.edn as edn])
   (:gen-class))
 
 (def grade-map {:A+ 4 :A 3.7 :B+ 3.3 :B 3 :C+ 2.7 :C 2.4 :D+ 2 :D 1.7 :F 0})
-(def sem1-courses {:AI01 { :grade "A"
-                      :hours 2} 
-              :AI02 { :grade "A+"
-                      :hours 3} 
-              :AI03 { :grade "D"
-                      :hours 2} 
-              :CS02 { :grade "C"
-                      :hours 2} 
-              :BI01 { :grade "B+"
-                      :hours 3}})
+(def sem1-courses {:AI01 {:grade "A"  :hours 2}
+                   :AI02 {:grade "A+" :hours 3}
+                   :AI03 {:grade "D"  :hours 2} 
+                   :CS02 {:grade "C"  :hours 2}
+                   :BI01 {:grade "B+" :hours 3}})
 
 (defn sem-gpa 
   "Calculate one semester GPA"
@@ -33,11 +29,11 @@
   (let [sum-gpas (->> gpas
                       (reduce +))
         divisor  (count gpas)]
-    (double (/ sum-gpas divisor))))
+    (format "%.3f" (double (/ sum-gpas divisor)))))
 
 (defn -main
   [& args] 
   (def gpa (sem-gpa sem1-courses))
   (println "Your GPA is: " (format "%.3f" gpa))
   (def cum-gpa (cumulative-gpa gpa 3.2 3.9))
-  (println "Your Cumulative GPA is: " (format "%.3f" cum-gpa)))
+  (println "Your Cumulative GPA is: " cum-gpa))
