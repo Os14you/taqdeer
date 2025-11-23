61950cee250108802fa72a79b72f59e4ff0fbded
Osama Yousef <osama.yousef@tuta.io>
2025-11-23 12:16:21 +0200

    feat: add cumulative gpa calculation feature
    New function added: `cumulative-gpa` which takes some number of GPAs
    and calculate the cumulative GPA from it.
    
    The logic: just sum up the GPAs and divide them by there sum.
    
    Author: @os14you

--- src/taqdeer/core.clj
+++ src/taqdeer/core.clj
@@ -2,7 +2,7 @@
   (:gen-class))
 
 (def grade-map {:A+ 4 :A 3.7 :B+ 3.3 :B 3 :C+ 2.7 :C 2.4 :D+ 2 :D 1.7 :F 0})
-(def courses {:AI01 { :grade "A"
+(def sem1-courses {:AI01 { :grade "A"
                       :hours 2} 
               :AI02 { :grade "A+"
                       :hours 3} 
@@ -14,6 +14,7 @@
                       :hours 3}})
 
 (defn sem-gpa 
+  "Calculate one semester GPA"
   [sem-courses]
   (let [total-points (->> sem-courses
                            (vals)
@@ -26,7 +27,17 @@
                           (reduce +))]
      (double (/ total-points total-hours))))
 
+(defn cumulative-gpa 
+  "Calculate the cumulative GPA, takes a list of GPAs"
+  [& gpas]
+  (let [sum-gpas (->> gpas
+                      (reduce +))
+        divisor  (count gpas)]
+    (double (/ sum-gpas divisor))))
+
 (defn -main
   [& args] 
-  (def gpa (sem-gpa courses))
-  (println "Your GPA is: " gpa))
+  (def gpa (sem-gpa sem1-courses))
+  (println "Your GPA is: " (format "%.3f" gpa))
+  (def cum-gpa (cumulative-gpa gpa 3.2 3.9))
+  (println "Your Cumulative GPA is: " (format "%.3f" cum-gpa)))
