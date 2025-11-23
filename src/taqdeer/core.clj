97ea49064bec7f1e0d4b8a2ab86ec87b3d81c827
Osama Yousef <osama.yousef@tuta.io>
2025-11-23 23:59:21 +0200

    feat: change core.clj to work with config.edn
    Add the main function `load-config` to handle loading the file, and
    another function that simplify the code `grade->points`.
    
    Now the user can change the config.edn instead.
    
    Author: @os14you

--- src/taqdeer/core.clj
+++ src/taqdeer/core.clj
@@ -1,21 +1,29 @@
 (ns taqdeer.core
-  (:require [clojure.edn as edn])
+  (:require [clojure.edn :as edn])
   (:gen-class))
 
-(def grade-map {:A+ 4 :A 3.7 :B+ 3.3 :B 3 :C+ 2.7 :C 2.4 :D+ 2 :D 1.7 :F 0})
 (def sem1-courses {:AI01 {:grade "A"  :hours 2}
                    :AI02 {:grade "A+" :hours 3}
                    :AI03 {:grade "D"  :hours 2} 
                    :CS02 {:grade "C"  :hours 2}
                    :BI01 {:grade "B+" :hours 3}})
 
+(defn load-config
+  [filename]
+  (edn/read-string (slurp filename)))
+
+(defn grade->points
+  [grade scale]
+  (let [grade-key (keyword grade)]
+    (get scale grade-key 0)))
+
 (defn sem-gpa 
   "Calculate one semester GPA"
-  [sem-courses]
+  [sem-courses grade-scale]
   (let [total-points (->> sem-courses
                            (vals)
                            (map (fn [{:keys [grade hours]}]
-                                  (* hours (get grade-map (keyword grade)))))
+                                  (* hours (grade->points grade grade-scale))))
                            (reduce +))
          total-hours (->> sem-courses
                           (vals)
@@ -33,7 +41,9 @@
 
 (defn -main
   [& args] 
-  (def gpa (sem-gpa sem1-courses))
+  (let [config (load-config "config.edn")
+        scale (:grade-scale config)]
+    (def gpa (sem-gpa sem1-courses scale)))
   (println "Your GPA is: " (format "%.3f" gpa))
   (def cum-gpa (cumulative-gpa gpa 3.2 3.9))
   (println "Your Cumulative GPA is: " cum-gpa))
